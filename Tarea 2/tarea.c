06b639346b91bac8839c2e04b0bc1b28567477e8
Andrés Cerda <a.cerda07@ufromail.cl>
2021-10-23 22:36:11 -0300

    tarea 2 con explicacion de pasos

--- Tarea 2/tarea.c
+++ Tarea 2/tarea.c
@@ -3,14 +3,15 @@
 int main () {
 
 
-int  Sala =20;
-int  Bodega =68;
-int producto =0;
-long usuario_trabajador0 =214275721;
-long usuario_trabajador =0;  
-long usuario_creado =0;
-long usuario_creado0 =0;
-int opcion=0;
+int  Sala = 20;
+int  Bodega = 68;
+int producto = 0;
+long usuario_trabajador0 = 214275721;
+long usuario_trabajador = 0;  
+long usuario_creado = 0;
+long usuario_creado0 = 0;
+int opcion = 0;
+int menu = 0;
 printf ("El Programa ha sido encendido\n");
 printf ("Hola bienvenido al sistema regulador de mercaderia\n");
 
@@ -18,14 +19,19 @@ printf ("Hola bienvenido al sistema regulador de mercaderia\n");
 printf ("En caso de tener usuario ingrese el numero 1 \nSi no tiene y desea uno, ingrese el numero 2\n");
 scanf("%d",&opcion);
 
+/* Se le pedira al usuario que ingrese una opcion, que en este caso serian 1 o 2 para que la funcion "switch" la lea */
+
 switch (opcion)
+
 {
-case 1 : printf("Ingrese su usuario, por favor\n");
+case 1 : /* a continuacion se hara una estructura if para cuando ingrese el usuario le diga si es correcto 
+        en caso de no serlo se dira que es incorrecto que le queda cierta cantidad de intentos hasta que se bloquee */
+         printf("Ingrese su usuario, por favor\n");
          scanf("%lu", &usuario_trabajador);
          
           if (usuario_trabajador0 == usuario_trabajador ) {
 
-            printf( "Usuario ingresado correctamente, Bienvenido\n");
+            printf( "Usuario ingresado correctamente, Bienvenido\n\n");
 
               }  else {
 
@@ -36,7 +42,7 @@ case 1 : printf("Ingrese su usuario, por favor\n");
     
                     if (usuario_trabajador0 == usuario_trabajador){
 
-                      printf("Usuario ingresado correctamente, Bienvenido\n");
+                      printf("Usuario ingresado correctamente, Bienvenido\n\n");
                 
                          } else { 
 
@@ -48,7 +54,7 @@ case 1 : printf("Ingrese su usuario, por favor\n");
 
                             if(usuario_trabajador0 == usuario_trabajador){ 
 
-                             printf("Usuario ingresado correctamente, Bienvenido\n");
+                             printf("Usuario ingresado correctamente, Bienvenido\n\n");
 
                              } else {  
 
@@ -69,14 +75,17 @@ case 1 : printf("Ingrese su usuario, por favor\n");
     
     break;
 
-case 2 : printf("Escriba su rut con digito verificador pero sin guion para crear su usuario\n");
+case 2 : /* aqui lo que se desea es crear el usuario, entonces se le pedira el rut sin guion y se le dara un valor 
+          para comprobar que no se equivoque se le pedira de nuevo pero ese valor se le asignara a otra variable para poder comparar */
+
+         printf("Escriba su rut con digito verificador pero sin guion para crear su usuario\n");
          scanf("%lu", &usuario_creado );
          printf("Usuario creado exitosamente\n");
          printf("Ingrese su usuario, por favor\n");
          scanf("%lu", &usuario_creado0);
          
          if (usuario_creado == usuario_creado0 ){ 
-             printf("Usuario ingresado correctamente\n");
+             printf("Usuario ingresado correctamente\n\n");
          
          } else {
              printf("recuerde que el usuario ingresado es %lu\n", usuario_creado);
@@ -84,7 +93,7 @@ case 2 : printf("Escriba su rut con digito verificador pero sin guion para crear
              scanf("%lu", &usuario_creado0);
              
              if(usuario_creado == usuario_creado0){
-                 printf("Usuario ingresado correctamente\n");
+                 printf("Usuario ingresado correctamente\n\n");
              } else {
                  printf("Dado que no puede ingresar correctamente el usuario creado el programa se apagara\n");
              }
@@ -93,11 +102,37 @@ case 2 : printf("Escriba su rut con digito verificador pero sin guion para crear
          }
     break;
 
-default : printf(" numero no valido\n"); 
+default : /* esta opcion es para limitar las opciones a 1 y 2 */
+  
+        printf(" numero no valido\n"); 
     
      break;
-           
-} if (opcion == 1 || opcion == 2) { printf(" Hola mundo gaturro"); }
+     
+ /* el if de a continuacion es para que al momento de que la opcion ingresada no sea 1 o 2, no siga con las demas intrucciones 
+ y solo se apague el programa*/          
+} if (opcion == 1 || opcion == 2) {
+
+    printf("-----------------------\n");
+    printf("---------Menu----------\n");
+    printf("-----------------------\n");
+    printf("1) venta de producto\n");
+    printf("2) editar inventario\n");
+    scanf("%d", &menu); 
+    
+    switch (menu)
+    {
+    case 1:
+        /* code */
+        break;
+
+    case 2: 
+
+
+    default:
+        break;
+    }
+
+}
 
 
 return 0;
