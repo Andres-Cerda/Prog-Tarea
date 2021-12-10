9bc84bb0c62dea4ad20cb2b4dcebcdf7126758ba
Andrés Cerda <a.cerda07@ufromail.cl>
2021-12-10 00:26:33 -0300

    Probando cosas

--- Probrando/probrando.c
+++ Probrando/probrando.c
@@ -1,51 +1,33 @@
 #include <stdio.h>
 
 int main(){
-
-    FILE * Basededatos;
+  // dat. para escribir en binario 
+    FILE * LeerCantidades = fopen ("Cantidad.dat", "r");
+    FILE * EscribirCantidades = fopen("Cantidad.dat", "a");
     char Codigo[20];
     char Producto[20];
     int  Cant_produto = 0;
-
-    int Cantidad_Por_Producto[1000];
-
-    Basededatos = fopen ("Datos.txt","r");
-    while ( fscanf(Basededatos,"%s %s",Codigo , Producto) == 2){
-
-                 Cant_produto++;
-                 printf("------ Codigo %s ------\n ", (int)Codigo);
-                 printf("Producto: %s \n Cantidad: %d \n ", Producto, Cantidad_Por_Producto[Cant_produto -1]);
-                 printf("--------------------------\n");
-
-                 printf("Desea modificar algun producto");
-                 scanf("%d", &Cant_produto - 1 );
-    }
-    fclose (Basededatos);
-      
-
-   
- 
-
-
-    /*Basededatos = fopen ("Datos.txt","r");
-    while ( fscanf(Basededatos,"%s %s %s",Codigo , Producto, Cantidad) == 3)
-    {
-                 
-                 Cant_produto++;
-                 printf("------ Producto %d ------\n ", Cant_produto);
-                 printf("Codigo: %s \n Producto: %s \n Cantidad: %s \n ",(int)Codigo, Producto, (int)Cantidad);
-                 printf("--------------------------\n");
-
-                  if(Cant_produto == 3 && (int)Codigo == 105){
-
-                  printf("Bien ahi");
-                  }
-                 
-            }
-    fclose (Basededatos); */
+    int  inicio = 0;
+    int  inicio2 = 0;
 
     
+    int Cantidad_Por_Productoss[1000];
+    rewind(LeerCantidades);
+  // hace que cada numero del .dat vaya a un arreglo
+    while ( fscanf(LeerCantidades, "%d ", &Cantidad_Por_Productoss[inicio]) == 1){
+          
+           printf("%d es la cantidad \n", Cantidad_Por_Productoss[inicio]);
+           inicio++;
+             
 
+    }
+
+    
+   // imprime un nuevo numero al arreglo
+    fprintf(EscribirCantidades, " %d", Cantidad_Por_Productoss[7] + 2);
+
+
+    fclose (LeerCantidades);
    
 
 
---
+++ sumatoria/Sumatoria.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "calculos.h"
+
+int main(){
+    
+    
+    int numeros[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    printf("Sumatoria: %i\n", sumatoriaArreglo(numeros, 5));
+    return 0;
+}
